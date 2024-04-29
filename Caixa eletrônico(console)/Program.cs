87dc9fa7dc7515cef4c76fd9c64cf551aaac43ae
ra2357083 <ra2357083@LDI317>
2024-04-29 09:27:29 -0300

    crediaria

--- Caixa eletrônico(console)/Program.cs
+++ Caixa eletrônico(console)/Program.cs
@@ -1,31 +1,76 @@
 ﻿using Caixa_eletrônico_console_;
 using System.Runtime.CompilerServices;
 
-CCorrente conta = new CCorrente("1111",500);
-CCorrente c2 = new CCorrente();
 
-void Extrato(CCorrente c)
+int menu()
 {
-    foreach (Transacao t in c.transacoes)
+    string? i;
+    Console.WriteLine("Digite o que deseja fazer:");
+    Console.WriteLine("Acesso administrativo (1)");
+    Console.WriteLine("Caixa elotronico (2)");
+    Console.WriteLine("Sair (3)");
+    i = Console.ReadLine();
+    int op;
+    Int32.TryParse(i, out op);
+    return op;
+}
+int esc1()
+{
+    int a = 1;
+    do
     {
-        Console.Write(t.tipo + " " + t.valor);
-        if(t.duplicata != null)
+        string? i;
+        Console.WriteLine("Digite o que deseja fazer:");
+        Console.WriteLine("cadastro de conta corrente (1)");
+        Console.WriteLine("Mostrar saldo de todas as contas (2)");
+        Console.WriteLine("Excluir conta específica (3)");
+        Console.WriteLine("Voltar (qualquer outro num)");
+        i = Console.ReadLine();
+        int op;
+        Int32.TryParse(i, out op);
+        switch (op)
         {
-            Console.WriteLine(" DUP: " + t.duplicata.valor + " " + t.duplicata.tipo);
+            case 1:
+                esc11();
+                break;
+            case 2:
+                esc12();
+                break;
+            case 3:
+                esc13();
+                break;
+            default:
+                a = 0;
+                break;
         }
-        else
-        {
-            Console.WriteLine();
-        }
-    }
-    Console.WriteLine("Saldo = " + c.saldo + "================");
+    } while (a != 0);
 }
 
-conta.Depositar(1000);
-conta.Transferir(c2, 100);
+int esc2()
+{
+    Console.WriteLine("Digite o numero da conta que deseja saber se existe");
+    int con;
+    string? i;
+    i = Console.ReadLine();
+    Int32.TryParse(i, out con);
+}
 
-Console.WriteLine("Extrato da c1: ");
-Extrato(conta);
 
-Console.WriteLine("Extrato da c2: ");
-Extrato(c2);
+int z = 1;
+do
+    switch (menu())
+    {
+        case 1:
+            esc1();
+            break;
+        case 2:
+            esc2();
+            break;
+        case 3:
+            Console.WriteLine("Adeus");
+            z = 0;
+            break;
+        default:
+            Console.WriteLine("Opção invalida");
+            break;
+    } while (z != 0);
